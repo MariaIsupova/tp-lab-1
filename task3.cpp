e547da08af8ceb89f894f0c819ec662a62c3b099
MariaIsupova <Mariaisisupova@gmail.com>
2018-10-23 22:52:33 +0300

    newtask1-3

---
+++ task3.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "task2.cpp"
+
+unsigned long long sumPrime(unsigned int hbound) {
+	unsigned int i;
+	unsigned long long sum=0;
+	for (i=2; i < hbound; i++) {
+		if (checkPrime(i) == 1) {
+			sum = sum + i;
+		}
+	}
+	return sum;
+}
