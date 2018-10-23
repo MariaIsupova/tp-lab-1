28c98154add3557184ad69d63a890500dd214370
MariaIsupova <Mariaisisupova@gmail.com>
2018-10-23 22:26:37 +0300

    change2

--- task1.cpp
+++ task1.cpp
@@ -1,18 +1,17 @@
-
 #include <iostream>
 
 unsigned long NOK(unsigned long long x, long y) {
 	long long a = x;
 	long b = y;
 	long res;
-		while (x != y) {
-			if (x > y) {
-				x = x - y;
-			}
-			else {
-				y = y - x;
-			}
+	while (x != y) {
+		if (x > y) {
+			x = x - y;
 		}
+		else {
+			y = y - x;
+		}
+	}
 	res = (a*b) / x;
 	return res;
 }
@@ -24,7 +23,7 @@ unsigned long findValue(unsigned int min, unsigned max)
 	k = min + 2;
 	res = NOK(min, min++);
 	for (int g = k; g <= max; g++) {
-		res = NOK(res,g);
+		res = NOK(res, g);
 	}
 	return res;
 }
